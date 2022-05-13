741b718f56341c3b2d719c2e39a95df19f3156d1
Dalia Mohamed <daliam7amad11@gmail.com>
2022-05-13 08:21:45 +0200

    project done!

--- server.js
+++ server.js
@@ -2,18 +2,44 @@
 projectData = {};
 
 // Require Express to run server and routes
-
+const express = require("express");
 // Start up an instance of app
+const app = express();
 
 /* Middleware*/
+const bodyParser = require('body-parser');
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
 
 // Initialize the main project folder
 app.use(express.static('website'));
 
 
-// Setup Server
+// Setup server
+// Callback function for debugging
+const port = 8000;
+const server = app.listen(port, ()=>{console.log(`Connected on port : ${port}`)});
+
+/* Initialize routes */
+
+// POST Route
+app.post('/postData',(request,respond)=>{
+    projectData={
+        temp:request.body.temp,
+        date:request.body.date,
+        feel:request.body.feel
+    }
+    respond.send(projectData)
+    console.log(projectData)   
+});
+
+// GET Route
+app.get('/getData',(request,respond)=>{
+    respond.send(projectData);
+});
+
--- website/app.js
+++ website/app.js
@@ -1,5 +1,71 @@
 /* Global Variables */
 
+// Personal API Key for OpenWeatherMap API
+const apiKey = '&appid=9f76e62168ca5ff8f168f2cfdea20553&units=imperial';
+
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let newDate = (d.getMonth()+1)+'.'+ d.getDate()+'.'+ d.getFullYear();
+
+
+// add event linser to generate button
+
+document.getElementById('generate').addEventListener('click',()=>{
+    let zipCode = document.getElementById('zip').value;
+    if(!zipCode || zipCode.length != 5){
+        //if the user didn't enter a valid zip code
+        alert("Please enter a valid zip code");
+    }else{
+        /*get data from the api using fetch function*/
+        const getData = async ()=>{
+            const request = await fetch(`https://api.openweathermap.org/data/2.5/weather?zip=${zipCode}${apiKey}`);
+            try {
+                const data = await request.json();
+                let feelings = document.getElementById('feelings').value;
+                console.log(`${data.main.temp},,${feelings},,${newDate}`);
+                postData('/postData',{temp:data.main.temp, date:newDate,feel:feelings});
+                retrieveData();
+            } catch (error) {
+                console.log(`Error Msg : ${error}`);
+            }
+        }
+        getData();
+    }
+
+});
+
+// POST data to server.js api
+const postData = async(url = '',data={})=>{
+    const response = await fetch(url,{
+        method:'POST',
+        credentials:'same-origin',
+        headers:{
+            'content-type':'application/json',
+        },
+        body:JSON.stringify(data),
+    });
+    try{
+        const getData = await response.json();
+        return getData;
+    }catch(error){
+        console.log("error",error);
+    }
+}
+
+// get data from my server.js api
+const retrieveData = async () =>{
+    const request = await fetch('/getData');
+    try {
+    // Transform into JSON
+    const allData = await request.json()
+    console.log(allData)
+    // Write updated data to DOM elements
+    document.getElementById('temp').innerHTML = Math.round(allData.temp)+ 'degrees';
+    document.getElementById('content').innerHTML = allData.feel;
+    document.getElementById("date").innerHTML =allData.date;
+    }
+    catch(error) {
+      console.log("error", error);
+      // appropriately handle the error
+    }
+   }
